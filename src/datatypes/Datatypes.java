32ab8e5aba4d2a4934143507bb5b689308f91ea1
haseeb <alamhaseeb036@gmail.com>
2020-10-16 12:22:28 +0500

    first project learning java

---
+++ src/datatypes/Datatypes.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class Datatypes {
+	public static void main(String args[]) {
+		
+//		int (integer ,number 5784)
+		
+		int a = 45;
+		int b = 200;
+		System.out.print("A and B After adding " + a + " " + b);
+	}
+
+}
---
+++ src/myFirst/MyFirst.java
@@ -0,0 +1,14 @@
+package myFirst;
+
+public class MyFirst {
+	
+	public static void main(String args[]) {
+		
+		System.out.print("heloo world \n");
+		System.out.println("heloo world");
+		
+		
+		
+	}
+
+}
